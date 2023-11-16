c764333abc5b807abf8b97628dc7034563225f2d
Copeq <mcxbbs@163.com>
2023-11-16 22:25:36 +0800

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -36,12 +36,12 @@ Typecho_Common::init();
 /** 定义数据库参数 */
 $db = new Typecho_Db('Pdo_Mysql', 'typecho_');
 $db->addServer(array (
-  'host' => '数据库地址',
-  'user' => '数据库用户名',
-  'password' => '数据库密码',
+  'host' => 'monorail.proxy.rlwy.net',
+  'user' => 'root',
+  'password' => 'EE1FecAdCBCE64gF3fddgd5Hd45Cf-Bh',
   'charset' => 'utf8mb4',
-  'port' => '3306',
-  'database' => '数据库昵称',
+  'port' => '29164',
+  'database' => 'railway',
   'engine' => 'MyISAM',
 ), Typecho_Db::READ | Typecho_Db::WRITE);
 Typecho_Db::set($db);
